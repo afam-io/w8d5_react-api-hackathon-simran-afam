439a64ceba66049826eda2fea925747ea87a1f69
AfamOkoh <afamokoh@gmail.com>
2022-06-17 14:25:24 +0100

    added some basic styling

--- joke-app/src/Components/App/index.js
+++ joke-app/src/Components/App/index.js
@@ -4,13 +4,11 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-
   // const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
   // const answer = await result.json();
   // console.log(answer.name);
   // setPokemon({ name: answer.name, imgSrc: answer.sprites.front_shiny });
 
- 
   const [joke, setJoke] = useState({
     joke: "get ready for the funniest joke of your life",
     answer: " ",
@@ -18,19 +16,20 @@ function App() {
 
   function handleClick() {
     async function fetchData() {
-    const result = await fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
-    const fetchJoke = await result.json()
-    setJoke({joke: fetchJoke.setup, answer: fetchJoke.delivery})
+      const result = await fetch("https://v2.jokeapi.dev/joke/Any?safe-mode");
+      const fetchJoke = await result.json();
+      setJoke({ joke: fetchJoke.setup, answer: fetchJoke.delivery });
     }
-   fetchData()
-
+    fetchData();
   }
 
   return (
     <main>
-    <h1>RANDOM JOKE GENERATOR</h1>
-      <Button handleClick={handleClick} />
-      <Display joke={joke}/>
+      <h1>RANDOM JOKE GENERATOR</h1>
+      <section className="bottom-display">
+        <Button handleClick={handleClick} />
+        <Display joke={joke} />
+      </section>
     </main>
   );
 }
